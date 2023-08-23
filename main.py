ff49a16c50b3d77a9f2d7fe495f144ffb8bc92d8
Aadi XP <89779399+bloored-chad@users.noreply.github.com>
2023-08-23 09:06:27 +0530

    main.py

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import discord
 from discord.ext import commands
 import os
 from googleapiclient.discovery import build
-
-#! Import this
+from config import api_key, cse_id, token
 import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = commands.Bot(command_prefix="$", intents=intents)
-api_key = "<YOUR CUSTOM SEARCH API KEY>"
-cse_id = "<YOUR PROJECT CSE ID>"
-token = "<YOUR DISCORD BOT TOKEN>"
 
 
 @client.event
